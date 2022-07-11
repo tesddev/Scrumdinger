672b91db521f6a1efe9950fdd990c48101d4322a
tesddev <tesleem.amuda@gmail.com>
2022-07-11 21:06:41 +0100

    add footer to page

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -14,12 +14,22 @@ struct MeetingView: View {
             HStack{
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
                 VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Button(action: {}){
+                    Image(systemName: "forward.fill")
                 }
             }
         }
