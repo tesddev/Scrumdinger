27f16a3d9d034749d7a28ffe9379fba35dddc583
tesddev <tesleem.amuda@gmail.com>
2022-07-14 22:22:17 +0100

    write code for preview for card view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -6,4 +6,16 @@
 //
 
 import Foundation
+import SwiftUI
 
+struct CardView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
