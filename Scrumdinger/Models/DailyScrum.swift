1dc634fc39c9027f00d19e2ae8addfb9d9b728a6
tesddev <tesleem.amuda@gmail.com>
2022-07-14 22:12:47 +0100

    create dailyScrum model

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,15 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by GIGL iOS on 14/07/2022.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
