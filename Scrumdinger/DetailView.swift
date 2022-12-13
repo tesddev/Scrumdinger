552e803e209b6f1d9b86f68b00a659bcf1b21147
tesddev <tesleem.amuda@gmail.com>
2022-12-13 10:45:17 +0100

    move detail view

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -8,13 +8,16 @@
 import SwiftUI
 
 struct DetailView: View {
+    let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        NavigationView{
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
     }
 }
