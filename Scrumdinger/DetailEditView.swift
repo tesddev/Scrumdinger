73d44d03fdc7939ffe5a29a87cf4e064a3b8d277
tesddev <tesleem.amuda@gmail.com>
2022-12-22 19:12:40 +0100

    replace the text body with a form with textfield

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -11,7 +11,11 @@ struct DetailEditView: View {
     @State private var data = DailyScrum.Data()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+            }
+        }
     }
 }
 
