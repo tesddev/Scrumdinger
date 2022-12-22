f149c238ccc8f6879aa281ba05fd1459abaf15d5
tesddev <tesleem.amuda@gmail.com>
2022-12-22 19:02:21 +0100

    add new view

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Tes on 22/12/2022.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
