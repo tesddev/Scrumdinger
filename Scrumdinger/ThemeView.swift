4848c5ef5c2d0e6053a6a2513d327fe6cb30b0f3
tesddev <tesleem.amuda@gmail.com>
2022-12-28 16:10:48 +0100

    add label to z stack and update theme to visible one

--- Scrumdinger/ThemeView.swift
+++ Scrumdinger/ThemeView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ThemeView: View {
     let theme: Theme
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 4)
+                .fill(theme.mainColor)
+            Label(theme.name, systemImage: "paintpalette")
+        }
     }
 }
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView(theme: .buttercup)
+        ThemeView(theme: .yellow)
     }
 }
