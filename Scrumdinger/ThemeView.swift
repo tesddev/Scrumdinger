238c78b5f40b37bce6ce175482b821ec593739b4
tesddev <tesleem.amuda@gmail.com>
2022-12-28 16:03:28 +0100

    Create a theme constant and pass a value in the preview

--- Scrumdinger/ThemeView.swift
+++ Scrumdinger/ThemeView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ThemeView: View {
+    let theme: Theme
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
@@ -15,6 +16,6 @@ struct ThemeView: View {
 
 struct ThemeView_Previews: PreviewProvider {
     static var previews: some View {
-        ThemeView()
+        ThemeView(theme: .buttercup)
     }
 }
