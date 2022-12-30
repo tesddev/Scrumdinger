b74b456841056e82fe6c09c184ad4c653bd1053a
tesddev <tesleem.amuda@gmail.com>
2022-12-30 11:55:28 +0100

    add conformance to CaseIterable and Identifiable, using the theme name as its id

--- Scrumdinger/Models/Themes.swift
+++ Scrumdinger/Models/Themes.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -56,5 +56,9 @@ enum Theme: String {
     
     var name: String {
         return rawValue.capitalized
+    }
+    
+    var id: String {
+        name
     }
 }
