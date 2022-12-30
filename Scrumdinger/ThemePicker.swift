68bddd31cc737f210e1fd5dd79bd010db134a764
tesddev <tesleem.amuda@gmail.com>
2022-12-30 11:56:18 +0100

    replace default textview with pickerview

--- Scrumdinger/ThemePicker.swift
+++ Scrumdinger/ThemePicker.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ThemePicker: View {
     @Binding var selection: Theme
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Picker("Theme", selection: $selection) {
+            
+        }
     }
 }
 
