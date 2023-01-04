88ff7c4c670de0da602622d775293ae22ba1150d
tesddev <tesleem.amuda@gmail.com>
2023-01-04 15:20:49 +0100

    create a new SwiftUI View file named MeetingHeaderView.swift

---
+++ Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by GIGL iOS on 04/01/2023.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView()
+    }
+}
