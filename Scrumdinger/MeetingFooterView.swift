8f6f071a3703742f998ce24de451ea44feead968
tesddev <tesleem.amuda@gmail.com>
2023-01-04 17:29:22 +0100

    create a new SwiftUI View file named MeetingFooterView.swift

---
+++ Scrumdinger/MeetingFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by GIGL iOS on 04/01/2023.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeetingFooterView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingFooterView()
+    }
+}
