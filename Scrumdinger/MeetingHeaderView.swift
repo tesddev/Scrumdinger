53f68e64676d9361a958540a0b3fccec8b2d366e
tesddev <tesleem.amuda@gmail.com>
2023-01-04 15:30:27 +0100

    create a top-level VStack and move the accessibility modifiers from the HStack to the VStack

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct MeetingHeaderView: View {
     var body: some View {
-        ProgressView(value: 5, total: 15)
-        HStack{
-            VStack(alignment: .leading) {
-                Text("Seconds Elapsed")
-                    .font(.caption)
-                Label("300", systemImage: "hourglass.bottomhalf.fill")
-            }
-            Spacer()
-            VStack(alignment: .trailing) {
-                Text("Seconds Remaining")
-                    .font(.caption)
-                Label("600", systemImage: "hourglass.tophalf.fill")
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
             }
         }
         .accessibilityElement(children: .ignore)
