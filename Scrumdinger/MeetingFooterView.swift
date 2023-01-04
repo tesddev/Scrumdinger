fd8253bdb13c29b32a4643428f944c1e60928546
tesddev <tesleem.amuda@gmail.com>
2023-01-04 17:36:39 +0100

    add a speakers property of type [ScrumTimer.Speaker]. In the preview, pass the MeetingFooterView initializer an array of speakers using the speakers property on DailyScrum.attendees

--- Scrumdinger/MeetingFooterView.swift
+++ Scrumdinger/MeetingFooterView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct MeetingFooterView: View {
+    let speakers: [ScrumTimer.Speaker]
     var body: some View {
         HStack {
             Text("Speaker 1 of 3")
@@ -22,6 +23,7 @@ struct MeetingFooterView: View {
 
 struct MeetingFooterView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingFooterView()
+        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers)
+            .previewLayout(.sizeThatFits)
     }
 }
