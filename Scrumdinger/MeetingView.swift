a8429fd46ea7edbc69b8c8065954e181c2ab6d07
tesddev <tesleem.amuda@gmail.com>
2023-01-04 17:27:42 +0100

    add an onDisappear modifier and call scrumTimer.stopScrum()

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -33,6 +33,9 @@ struct MeetingView: View {
                 scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
                 scrumTimer.startScrum()
             }
+            .onDisappear{
+                scrumTimer.stopScrum()
+            }
             .navigationBarTitleDisplayMode(.inline)
         }
     }
