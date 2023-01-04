d418a8472bde80acadd72e38352556e950537c4b
tesddev <tesleem.amuda@gmail.com>
2023-01-04 17:56:28 +0100

    add a call to the MeetingFooterView, passing speakers and the skip action

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -18,6 +18,9 @@ struct MeetingView: View {
                 MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
                 Circle()
                     .strokeBorder(lineWidth: 24)
+                MeetingFooterView(speakers: scrumTimer.speakers) {
+                    scrumTimer.skipSpeaker()
+                }
             }
             .padding()
             .foregroundColor(scrum.theme.accentColor)
