11e5e574cc4c3c42f0a5a4f352ce9132a25ef29e
tesddev <tesleem.amuda@gmail.com>
2023-01-04 17:37:50 +0100

    add a skipAction closure property to the view and pass an empty closure to the MeetingFooterView initializer in the preview

--- Scrumdinger/MeetingFooterView.swift
+++ Scrumdinger/MeetingFooterView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MeetingFooterView: View {
     let speakers: [ScrumTimer.Speaker]
+    var skipAction: () -> Void
     var body: some View {
         HStack {
             Text("Speaker 1 of 3")
@@ -23,7 +24,7 @@ struct MeetingFooterView: View {
 
 struct MeetingFooterView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers)
+        MeetingFooterView(speakers: DailyScrum.sampleData[0].attendees.speakers, skipAction: {})
             .previewLayout(.sizeThatFits)
     }
 }
