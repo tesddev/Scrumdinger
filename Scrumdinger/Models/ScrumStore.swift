541bbea96702007f9dc083b8b6d774fd4d9df96e
tesddev <tesleem.amuda@gmail.com>
2023-01-09 18:41:38 +0100

    add a private static throwing function named fileURL that returns a URL

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -10,4 +10,11 @@ import SwiftUI
 
 class ScrumStore: ObservableObject {
     @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+    }
 }
