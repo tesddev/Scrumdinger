697d3f0421120d863e1974a6babf159763c905d7
tesddev <tesleem.amuda@gmail.com>
2023-01-21 19:24:35 +0100

    pass the error wrapper to the ErrorView initializer

--- Scrumdinger/ErrorView.swift
+++ Scrumdinger/ErrorView.swift
@@ -5,5 +5,24 @@
 //  Created by Tes on 21/01/2023.
 //
 
-import Foundation
+import SwiftUI
 
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
